a321e6980b0415cbfad5609e2ebf1ca4c6cc1be9
Haotong0509 <xuhaotong0509@gmail.com>
2023-02-15 14:13:24 +0800

    optimize some functions

--- src/extension.ts
+++ src/extension.ts
@@ -15,13 +15,12 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('attention-highlight.helloWorld', () => {
+	let disposable = vscode.commands.registerCommand('attention-highlight.staticHighlight', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World!');
-		//highlightTest();
+		//openFile('/Users/haotong/attention-highlight/sample.py');
 		let weightData = parseWeight('/Users/haotong/attention-highlight/attention_weight.json');
-		//vscode.window.showInformationMessage(weightData);
 		//console.log(weightData);
 		highlightTarget(weightData);
 	});
@@ -29,31 +28,43 @@ export function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(disposable);
 }
 
+// export async function openFile(path: string) {
+// 	let fullPath = vscode.Uri.parse(path);
+// 	await vscode.workspace.openTextDocument(fullPath)
+// 	.then(async doc => {
+// 		vscode.window.showTextDocument(doc, undefined, false);
+// 	});
+// }
 
-export async function highlightTest() {
-	// 转换路径
-	let fullPath = vscode.Uri.parse('/Users/haotong/attention-highlight/sample.py');
-	let lineNumber = 2;
-	// 打开文件
-	await vscode.workspace.openTextDocument(fullPath)
-	.then(async doc => {
-	  // 获取当前打开的文件的editor 
-	  //vscode.window.showTextDocument(doc);
-	  let editor = vscode.window.activeTextEditor;
-	  if (!editor) {
-		return;
-	  }
-	  //从editor 中拿document。
-	  let document = editor.document;
-	  // 调用document的lineAt获取某行代码的range。
-	  let start = document.lineAt(lineNumber).range.start.character;
-	  let end = document.lineAt(lineNumber).range.end.character;
-	  // 组装range。
-	  let range: vscode.Range = new vscode.Range(lineNumber, start, lineNumber, end);
-	  //高亮！！！
-	  await vscode.window.showTextDocument(doc , {preserveFocus: false, selection: range, viewColumn: vscode.ViewColumn.One});
-	});
+export function openFile(path: string) {
+	let fullPath = vscode.Uri.parse(path);
+	vscode.workspace.openTextDocument(fullPath).then(doc => vscode.window.showTextDocument(doc));
 }
+
+// export async function highlightTest() {
+// 	// 转换路径
+// 	let fullPath = vscode.Uri.parse('/Users/haotong/attention-highlight/sample.py');
+// 	let lineNumber = 2;
+// 	// 打开文件
+// 	await vscode.workspace.openTextDocument(fullPath)
+// 	.then(async doc => {
+// 	  // 获取当前打开的文件的editor 
+// 	  //vscode.window.showTextDocument(doc);
+// 	  let editor = vscode.window.activeTextEditor;
+// 	  if (!editor) {
+// 		return;
+// 	  }
+// 	  //从editor 中拿document。
+// 	  let document = editor.document;
+// 	  // 调用document的lineAt获取某行代码的range。
+// 	  let start = document.lineAt(lineNumber).range.start.character;
+// 	  let end = document.lineAt(lineNumber).range.end.character;
+// 	  // 组装range。
+// 	  let range: vscode.Range = new vscode.Range(lineNumber, start, lineNumber, end);
+// 	  //高亮！！！
+// 	  await vscode.window.showTextDocument(doc , {preserveFocus: false, selection: range, viewColumn: vscode.ViewColumn.One});
+// 	});
+// }
 
 // This method is called when your extension is deactivated
 export function deactivate() {}
--- src/highlight.ts
+++ src/highlight.ts
@@ -9,7 +9,7 @@ type StaticWeight = {
   };
 
 var background:string[]; 
-background = ["#965454","#FFFFe5","#FFFFcc","#FFFF00","#3072f6","#3072f6","#FFFFe5","#FFFFcc","#FFFFcc","#FFFF00","#FFFF00"];
+background = ["#965454","#FFFFe5","#FFFFcc","#d2d22d","#d9d926","#dfdf20","#e5e619","#ecec13","#f2f20d","#f9f906","#FFFF00"];
 
 export const highlightTarget = (obj: object) => {
 
@@ -37,19 +37,33 @@ export const highlightWord = (staticWeight: StaticWeight) => {
     });
 
     // 获取可见编辑器实例
-    const visibleTextEditors = vscode.window.visibleTextEditors;
-    visibleTextEditors.forEach((editor) => {
+    let editor = vscode.window.activeTextEditor;
+    //console.log(editor);
+    if (!editor) {
+        console.log("No open text editor");
+ 		return;
+ 	}
+    let document = editor.document;
+    // 通过文档实例获取目标字符串位置
+    const startPos = document.positionAt(startIndex);
+    const endPos = document.positionAt(endIndex+1);
+    const range = [new vscode.Range(startPos, endPos)];
+    // 在目标位置设置装饰器
+    editor.setDecorations(decorator, range);
 
-        // 获取文档实例
-        const { document } = editor;
+    // const visibleTextEditors = vscode.window.visibleTextEditors;
+    // visibleTextEditors.forEach((editor) => {
 
-        // 通过文档实例获取目标字符串位置
-        //const startIndex = document.getText().indexOf(str);
-        const startPos = document.positionAt(startIndex);
-        const endPos = document.positionAt(endIndex+1);
-        const range = [new vscode.Range(startPos, endPos)];
+    //     // 获取文档实例
+    //     const { document } = editor;
 
-        // 在目标位置设置装饰器
-        editor.setDecorations(decorator, range);
-    });
+    //     // 通过文档实例获取目标字符串位置
+    //     //const startIndex = document.getText().indexOf(str);
+    //     const startPos = document.positionAt(startIndex);
+    //     const endPos = document.positionAt(endIndex+1);
+    //     const range = [new vscode.Range(startPos, endPos)];
+
+    //     // 在目标位置设置装饰器
+    //     editor.setDecorations(decorator, range);
+    // });
 };
